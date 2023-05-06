4cd581cf3e0e5ea7df7cee42add26f4ee25ba8fa
KONTROLCHEK <130096547+KONTROLCHEK@users.noreply.github.com>
2023-05-06 15:49:25 +0300

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,27 @@
+language = 'ru'
+chat = '5897642255'
+chat1 = '5312252852'
+kamen = "Камень"
+noshik = "Ножницы"
+bumaga = "Бумага"
+stop = "/stop"
+kanal = "@kontrol_chek185"
+
+shet = {}
+
+text_photo = "*Важно! Что такое Chat Id и как его получить?*\nChat Id - это уникальный идентификатор, который используется в ботах для отправки сообщений пользователям (надеюсь объяснил понятно).\nДля того чтобы получить Chat Id вашего друга нужно найти сообщение друга и отправить его боту @userinfobot и скопировать набор цифр из *ВТОРОЙ СТРОКИ* и отправить его в ответ на это сообщение!\n_P.S. кнопка /stop это для того чтобы отменить выбор!_"
+bot_knb_text = ""
+user_message = ""
+bot_knb1 = [kamen, noshik, bumaga, stop]
+bot_knb = ["Камень", "Ножницы", "Бумага"]
+kmn_reshim = ["Одиночный", "Сетевой"]
+admin_chat = [chat, chat1]
+slova = ["Да!", "Нет!"]
+blogers = ["KONTROL CHEK", "IlyuhichPLAY"]
+slo1 = ["Крутить", "Удалить", "Завершить"]
+slo1_1 = ["Крутить", "Завершить"]
+kluchi_shifron = ["24gjshtu35", "7kd66sgsty", "dgnhj246cx", "124hgjydss", "78e4gaytre", "232ccxuk44", "56njhftttt", "445gfsytrw", "086chgdfdd", "1233ffyrey", "rrw444dksh", "gfrrgr4566", "qw434gfh34", "mnv664ngbx", "kjyu562gse", "rytjhg3442", "klhopg4543", "243hgs6uyu", "7kjg8l7g8", "gra412grwr", "rhteyh2y6j", "h54u2u65j", "g525y5h52", "4y52jhj62", "h264u632uh", "6423ujj62", "jnyj475hg", "kkuukjuo6", "kj47uku6847", "ujtkjrutkur", "m4k675um745", "rg2hth325", "btwh6jh63", "tw365h36h", "n6356j35jyhg", "ytreukut5", "jyeje6u47", "jtryyurkj4", "jyteyjj547", "aeqwtgt4rq", "qgrgqwehw24", "52htrhytrh", "35htjhyrj6jy", "rsjjyeyuy543", "h645u6u32j63", "yjejytj7kj6", "wrjywjwuy", "tjh6j356iy", "agtqty456n", "kulilio88lo", "jhfbgfhgg7", "45ythyth6ru6u", "thytjyjy6h4", "thhjjdytjy656", "gfhtjht432"]
+vib = ["Да!", "Нет!"]
+vib1 = ["Отменить игру", "Пригласить другого пользователя", "Играть в двоем"]
+kmn_kolvo = ["2 игрока", "3 игрока"]
+slova2 = ["Передать возможность добавить второму игроку нужное значение", "Передать возможность добавить первому игроку нужное значение", "/stop"]
